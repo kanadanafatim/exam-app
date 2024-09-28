76e97fb9b6db373f6e395c00e29c5c502fe52235
Kanadana F. <aalyahfatim@gmail.com>
2024-09-28 21:24:20 +0000

    add comments to code

--- src/com/something/controllers/BaseController.java
+++ src/com/something/controllers/BaseController.java
@@ -16,19 +16,23 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class BaseController {
-	
-	private String uri = "jdbc:mysql://ls-0f19f4268096a452a869b6f8467bc299c51da519.cz6cgwgke8xd.eu-west-3.rds.amazonaws.com:3306/db0072493";
-	private String user = "user0072493";
-	private String password = "Yf3IgyBsOPa34WR";
-	protected Double rate = 0.0;
-	protected DecimalFormat df = new DecimalFormat("#.##");
-	
-	protected Alert alert;
-	protected Data data;
 
+    // Détails de la connexion à la base de données
+    private String uri = "jdbc:mysql://ls-0f19f4268096a452a869b6f8467bc299c51da519.cz6cgwgke8xd.eu-west-3.rds.amazonaws.com:3306/db0072493";
+    private String user = "user0072493";
+    private String password = "Yf3IgyBsOPa34WR";
 
-	protected Connection connection() {
-		Connection connexion = null;
+    // Variables pour stocker le taux de réussite et le format décimal
+    protected Double rate = 0.0;
+    protected DecimalFormat df = new DecimalFormat("#.##");
+
+    // Objets Alert et Data
+    protected Alert alert;
+    protected Data data;
+
+    // Méthode pour établir une connexion à la base de données
+    protected Connection connection() {
+        Connection connexion = null;
 
         try {
             connexion = DriverManager.getConnection(uri, user, password);
@@ -38,75 +42,78 @@ public class BaseController {
         }
 
         return connexion;
-	}
+    }
 
-	protected void fetch() {
-		Double total = 0.0;
-		int totalSuccess = 0;
+    // Méthode pour récupérer les données de la base de données et calculer le taux de réussite
+    protected void fetch() {
+        Double total = 0.0;
+        int totalSuccess = 0;
         Connection connection = connection();
 
         if (connection != null) {
             String query = "SELECT * FROM resultat";
             try (Statement stmt = connection.createStatement();
-            		ResultSet rs = stmt.executeQuery(query)) {
+                 ResultSet rs = stmt.executeQuery(query)) {
                 while (rs.next()) {
                     Boolean statut = rs.getBoolean("statut");
 
-                    total ++;
-                    if (statut) totalSuccess ++;
+                    total++;
+                    if (statut) totalSuccess++;
                 }
                 connection.close();
             } catch (SQLException e) {
                 System.out.println("Error fetching data: " + e.getMessage());
             }
         }
-        
-        rate = (total / totalSuccess) * 100;
-    }
-	
-	protected Boolean getResult(String matricule) {
-		Connection connection = connection();
-		
-		if (connection != null) {
-			String query = "SELECT * FROM resultat INNER JOIN etudiant "
-					+ "WHERE resultat.matricule_etudiant = etudiant.matricule "
-					+ "AND resultat.matricule_etudiant = ?";
-			try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-				pstmt.setString(1, matricule);
-				ResultSet rs = pstmt.executeQuery();
-				
-				if (rs.next()) {
-					data = new Data();
-					data.setNom(rs.getString("nom"));
-					data.setPnom(rs.getString("prenoms"));
-					data.setMatricule(rs.getString("matricule"));
-					data.setDate_naissance(rs.getString("date_naissance"));
-					data.setEcole(rs.getString("ecole"));
-					data.setMoyenne(rs.getDouble("moyenne"));
-					data.setStatut_examen(rs.getBoolean("statut"));
 
-					return true;
-				} else {
-					alert = new Alert(AlertType.INFORMATION);
-					alert.setTitle("Information");
-					alert.setContentText("Matricule non reconnu.");
-				}
-			} catch (SQLException e) {
-				alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Erreur");
-				alert.setContentText("Erreur lors de l'exécution de la requête.\n"
-						+ "Message: " + e.getMessage());
-			}
-		}
-		
-		return false;
-	}
-	
-	protected void navigate(AnchorPane ecran, List<AnchorPane> listEc) {
-		for (AnchorPane item : listEc) {
-			item.setVisible(false);
-		}
-		
-		ecran.setVisible(true);
-	}
+        rate = (totalSuccess / total) * 100;
+    }
+
+    // Méthode pour récupérer les données de résultat de l'étudiant en fonction du numéro de matricule
+    protected Boolean getResult(String matricule) {
+        Connection connection = connection();
+
+        if (connection != null) {
+            String query = "SELECT * FROM resultat INNER JOIN etudiant "
+                    + "WHERE resultat.matricule_etudiant = etudiant.matricule "
+                    + "AND resultat.matricule_etudiant = ?";
+            try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+                pstmt.setString(1, matricule);
+                ResultSet rs = pstmt.executeQuery();
+
+                if (rs.next()) {
+                    data = new Data();
+                    data.setNom(rs.getString("nom"));
+                    data.setPnom(rs.getString("prenoms"));
+                    data.setMatricule(rs.getString("matricule"));
+                    data.setDate_naissance(rs.getString("date_naissance"));
+                    data.setEcole(rs.getString("ecole"));
+                    data.setMoyenne(rs.getDouble("moyenne"));
+                    data.setStatut_examen(rs.getBoolean("statut"));
+
+                    return true;
+                } else {
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information");
+                    alert.setContentText("Matricule non reconnu.");
+                }
+            } catch (SQLException e) {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Erreur");
+                alert.setContentText("Erreur lors de l'exécution de la requête.\n"
+                        + "Message: " + e.getMessage());
+            }
+        }
+
+        return false;
+    }
+
+    // Méthode pour naviguer entre différents écrans de l'application
+    protected void navigate(AnchorPane ecran, List<AnchorPane> listEc) {
+        for (AnchorPane item : listEc) {
+            item.setVisible(false);
+        }
+
+        ecran.setVisible(true);
+    }
 }
--- src/com/something/controllers/ui/ScreenController.java
+++ src/com/something/controllers/ui/ScreenController.java
@@ -1,7 +1,6 @@
 package com.something.controllers.ui;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import com.something.controllers.BaseController;
@@ -20,136 +19,154 @@ import javafx.scene.paint.Color;
 
 public class ScreenController extends BaseController implements Initializable {
 
-	@FXML
-	private Label labelPct;
+    // Champs annotés FXML qui correspondent aux éléments de l'interface utilisateur dans le fichier FXML
+    @FXML
+    private Label labelPct;
 
-	@FXML
-	private TextField champMatricule;
+    @FXML
+    private TextField champMatricule;
 
-	@FXML
-	private Label statutResultat;
-	
-	@FXML
-	private Label etudiantNomPrenoms;
-	
-	@FXML
-	private Label etudiantMatricule;
-	
-	@FXML
-	private Label etudiantDateNaissance;
-	
-	@FXML
-	private Label etudiantEcole;
+    @FXML
+    private Label statutResultat;
 
-	@FXML
-	private Label moyenneEt;
-	
-	@FXML
-	private AnchorPane ecranAccueil;
-	
-	@FXML
-	private AnchorPane ecranRecherche;
-	
-	@FXML
-	private AnchorPane conteneurApresRecherche;
-	
-	@FXML
-	private AnchorPane ecranDetails;
+    @FXML
+    private Label etudiantNomPrenoms;
 
-	
-	ObservableList<AnchorPane> listEcrans;
+    @FXML
+    private Label etudiantMatricule;
 
-	@FXML
-	private void afficherPageRecherche() {
-		navigate(ecranRecherche, listEcrans);
-	}
-	
-	@FXML
-	private void afficherPageAccueil() {
-		navigate(ecranAccueil, listEcrans);
-	}
-	
-	@FXML
-	private void afficherPageDetails() {
-		etudiantNomPrenoms.setText(String.format("%s %s", data.getNom(), data.getPnom()));
-		etudiantDateNaissance.setText(data.getDate_naissance());
-		etudiantMatricule.setText(data.getMatricule());
-		etudiantEcole.setText(data.getEcole());
-		moyenneEt.setText(String.valueOf(data.getMoyenne()));
-		
-		navigate(ecranDetails, listEcrans);
-	}
-	
-	
-	// Actions
-	@FXML
-	private void rechercherResultat() {
-		if (champMatricule.getText() == null || champMatricule.getText().isEmpty()) {
-			alert = new Alert(AlertType.WARNING);
-			alert.setTitle("Avertissement");
-			alert.setContentText("Veuillez renseigner votre matricule");
-		} else {
-			try {
-				Integer.valueOf(champMatricule.getText());
-				String matricule = champMatricule.getText();
+    @FXML
+    private Label etudiantDateNaissance;
 
-				if (getResult(matricule)) {
-					setTextColor(moyenneEt, data.getMoyenne(), true);
-					setTextColor(statutResultat, data.getStatut_examen(), false);
+    @FXML
+    private Label etudiantEcole;
 
-					afficherConteneurApresRecherche();
-				}
-			} catch (NumberFormatException e) {
-				alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Erreur");
-				alert.setContentText("Vous devez renseigner que des nombres");
-			}
-		}
+    @FXML
+    private Label moyenneEt;
 
-		if (alert != null) {
-			alert.showAndWait();
-		}
-	}
+    @FXML
+    private AnchorPane ecranAccueil;
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		fetch();
-		
-		listEcrans = FXCollections.observableArrayList(
-			ecranAccueil, ecranRecherche, ecranDetails
-		);
+    @FXML
+    private AnchorPane ecranRecherche;
 
-		labelPct.setText(df.format(rate) + " %");
-		
-		setTextColor(labelPct, rate, false);
-	}
+    @FXML
+    private AnchorPane conteneurApresRecherche;
 
-	private <T> void setTextColor(Label label, T value, Boolean isAvg) {
-		if (value instanceof Double && !isAvg) {
-			if ((Double) value >= 50) {
-				label.setTextFill(Color.valueOf("#4caf50"));
-			} else {
-				label.setTextFill(Color.valueOf("#c70000"));
-			}
-		} else if (value instanceof Double && isAvg) {
-			if ((Double) value >= 10) {
-				label.setTextFill(Color.valueOf("#4caf50"));
-			} else {
-				label.setTextFill(Color.valueOf("#c70000"));
-			}
-		} else if (value instanceof Boolean) {
-			if ((Boolean) value == true) {
-				label.setTextFill(Color.valueOf("#4caf50"));
-			} else {
-				label.setTextFill(Color.valueOf("#c70000"));
-			}
-		}
-	}
+    @FXML
+    private AnchorPane ecranDetails;
 
-	private void afficherConteneurApresRecherche() {
-		if (data != null) {
-			statutResultat.setText(data.getStatut_examen() ? "Admis(e)" : "Refusé(e)");
-			conteneurApresRecherche.setVisible(true);
-		}
-	}
+    // Liste observable d'AnchorPane pour l'écran d'accueil, l'écran de recherche et l'écran de détails
+    ObservableList<AnchorPane> listEcrans;
+
+    // Méthode appelée lorsque l'utilisateur clique sur le bouton "Recherche" dans l'écran d'accueil
+    @FXML
+    private void afficherPageRecherche() {
+        navigate(ecranRecherche, listEcrans);
+    }
+
+    // Méthode appelée lorsque l'utilisateur clique sur le bouton "Accueil" dans l'écran de recherche
+    @FXML
+    private void afficherPageAccueil() {
+        navigate(ecranAccueil, listEcrans);
+    }
+
+    // Méthode appelée lorsque l'utilisateur clique sur le bouton "Détails" dans l'écran de recherche
+    @FXML
+    private void afficherPageDetails() {
+        // Définit le texte des étiquettes dans l'écran de détails pour afficher les données de l'étudiant
+        etudiantNomPrenoms.setText(String.format("%s %s", data.getNom(), data.getPnom()));
+        etudiantDateNaissance.setText(data.getDate_naissance());
+        etudiantMatricule.setText(data.getMatricule());
+        etudiantEcole.setText(data.getEcole());
+        moyenneEt.setText(String.valueOf(data.getMoyenne()));
+
+        // Navigue vers l'écran de détails
+        navigate(ecranDetails, listEcrans);
+    }
+
+    // Méthode appelée lorsque l'utilisateur clique sur le bouton "Rechercher" dans l'écran de recherche
+    @FXML
+    private void rechercherResultat() {
+        // Vérifie si le numéro de matricule entré par l'utilisateur est valide
+        if (champMatricule.getText() == null || champMatricule.getText().isEmpty()) {
+            alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Avertissement");
+            alert.setContentText("Veuillez renseigner votre matricule");
+        } else {
+            try {
+                Integer.valueOf(champMatricule.getText());
+                String matricule = champMatricule.getText();
+
+                // Récupère les données de résultat de l'étudiant en utilisant la méthode getResult()
+                if (getResult(matricule)) {
+                    // Définit la couleur du texte des étiquettes en fonction de la moyenne de l'étudiant et de son statut d'examen
+                    setTextColor(moyenneEt, data.getMoyenne(), true);
+                    setTextColor(statutResultat, data.getStatut_examen(), false);
+
+                    // Affiche les données de l'étudiant dans l'écran de détails
+                    afficherConteneurApresRecherche();
+                }
+            } catch (NumberFormatException e) {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Erreur");
+                alert.setContentText("Vous devez renseigner que des nombres");
+            }
+        }
+
+        // Affiche l'alerte s'il y a une erreur ou un avertissement
+        if (alert != null) {
+            alert.showAndWait();
+        }
+    }
+
+    // Méthode appelée après que le fichier FXML a été chargé
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // Récupère les données nécessaires à l'application
+        fetch();
+
+        // Initialise la liste observable d'AnchorPane
+        listEcrans = FXCollections.observableArrayList(
+                ecranAccueil, ecranRecherche, ecranDetails
+        );
+
+        // Définit le texte de l'étiquette labelPct au taux de réussite, formaté en pourcentage
+        labelPct.setText(df.format(rate) + " %");
+
+        // Définit la couleur du texte de l'étiquette labelPct en fonction du taux de réussite
+        setTextColor(labelPct, rate, false);
+    }
+
+    // Méthode générique qui définit la couleur du texte d'une étiquette en fonction de la valeur d'un objet donné
+    private <T> void setTextColor(Label label, T value, Boolean isAvg) {
+        if (value instanceof Double && !isAvg) {
+            if ((Double) value >= 50) {
+                label.setTextFill(Color.valueOf("#4caf50"));
+            } else {
+                label.setTextFill(Color.valueOf("#c70000"));
+            }
+        } else if (value instanceof Double && isAvg) {
+            if ((Double) value >= 10) {
+                label.setTextFill(Color.valueOf("#4caf50"));
+            } else {
+                label.setTextFill(Color.valueOf("#c70000"));
+            }
+        } else if (value instanceof Boolean) {
+            if ((Boolean) value == true) {
+                label.setTextFill(Color.valueOf("#4caf50"));
+            } else {
+                label.setTextFill(Color.valueOf("#c70000"));
+            }
+        }
+    }
+
+    // Méthode qui définit la visibilité de l'AnchorPane conteneurApresRecherche à true
+    // et définit le texte de l'étiquette statutResultat au statut d'admission de l'étudiant
+    private void afficherConteneurApresRecherche() {
+        if (data != null) {
+            statutResultat.setText(data.getStatut_examen() ? "Admis(e)" : "Refusé(e)");
+            conteneurApresRecherche.setVisible(true);
+        }
+    }
 }
